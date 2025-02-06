7cd2935c70b897116a81df47af86c1d262383b7e
Hossein <Hossein.kelisa@gmail.com>
2025-02-06 15:30:01 +0100

    add constants.js

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const BODY = document.body;
+// App texts
+export const TITLE_TEXT = "English Dictionary";
+export const INPUT_PLACEHOLDER = "Enter a word...";
+export const BUTTON_TEXT = "Search";
+
+// Result labels
+export const MEANING_LABEL_TEXT = "Meaning:";
+export const SYNONYM_LABEL_TEXT = "Synonym:";
