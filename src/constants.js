73321682736e34da3607b646e319e05beab9a6c5
Hossein <Hossein.kelisa@gmail.com>
2025-02-08 20:28:49 +0100

    Adding API for Dictionary

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,10 @@ export const BUTTON_TEXT = "Search";
 // Result labels
 export const MEANING_LABEL_TEXT = "Meaning:";
 export const SYNONYM_LABEL_TEXT = "Synonym:";
+
+// Default values for when no results are found
+export const DEFAULT_MEANING = "Oops... No meaning found.";
+export const DEFAULT_SYNONYM = "Oops... No synonyms found.";
+
+// API Endpoint (Optional)
+export const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
